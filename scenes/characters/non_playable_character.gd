c6c6a6c684f9fd6370e5710702d66c29e5408b41
MangoFloat17 <50304360+MangoFloat17@users.noreply.github.com>
2025-12-14 13:58:15 -0600

    Add files via upload

---
+++ scenes/characters/non_playable_character.gd
@@ -0,0 +1,15 @@
+class_name NonPlayableCharacter
+extends CharacterBody2D
+
+@export var min_walk_cycle: int=1
+@export var max_walk_cycle: int=4
+
+@export var level: int=1
+@export var speed:float=20
+@export var maxHealth:int=1
+@export var hitDamage:int=1
+@export var currentHealth:int
+var facing:float
+var walk_cycles:int
+var current_walk_cycle:int
+@export var target:Node2D
